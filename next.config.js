510020ca1357c8abd16d6c2129eea9d733c0e7b0
tradebitsix <147550839+tradebitsix@users.noreply.github.com>
2026-01-01 10:15:47 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { appDir: true },
-  reactStrictMode: true,
-};
-module.exports = nextConfig;
+  reactStrictMode: true
+}
+
+export default nextConfig
